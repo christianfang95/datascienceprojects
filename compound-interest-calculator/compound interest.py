a7c521b828372f71ff288f71f042577636a1a8fe
Christian Fang <c.zielinski@students.uu.nl>
2022-09-01 16:04:04 +0200

    Def version

--- compound-interest-calculator/compound interest.py
+++ compound-interest-calculator/compound interest.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 
 #Set global parameters
 P = 0 # The Starting Principal
@@ -9,7 +10,7 @@ r = 0.05 # The Annual Interest Rate to Compound On 7%
 t = 30 # The Number of Years to Compound
 M = 600 # Monthly Contribution Amount
 
-#Compound interest with monthly contributions
+#Calculate compound interest with monthly contributions
 results = pd.DataFrame(columns = ['Month', 'Invested', 'Value'])
 
 for i in range(1, t * 12 + 1):
@@ -21,31 +22,31 @@ for i in range(1, t * 12 + 1):
     Value = P * ((1 + (r / 12)) ** i) + M * ((((1 + r / 12) ** (i + 1)) - (1 + (r / 12))) / (r / 12))
   results =  results.append({'Month': Month, 'Invested': Invested, 'Value': Value}, ignore_index = True)
 
-#plot
+#Plot compound interest rate with monthly contributions
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=results["Month"], y=results["Invested"],
-                    mode='lines',
-                    name='Invested Capital'))
-fig.add_trace(go.Scatter(x=results["Month"], y=results["Value"],
-                    mode='lines',
-                    fill='tonexty',
-                    name='Value of Investment'))
-fig.update_layout(title='Compound Interest',
-                   xaxis_title='Months',
-                   yaxis_title='Amount',
-                   font=dict(
-        family="Helvetica",
-        size=12
+fig.add_trace(go.Scatter(x = results["Month"], y = results["Invested"],
+                    mode = 'lines',
+                    name = 'Invested Capital'))
+fig.add_trace(go.Scatter(x=results["Month"], y = results["Value"],
+                    mode = 'lines',
+                    fill = 'tonexty',
+                    name = 'Value of Investment'))
+fig.update_layout(title = 'Compound Interest',
+                   xaxis_title = 'Months',
+                   yaxis_title = 'Amount',
+                   font = dict(
+                        family = "Helvetica",
+                        size = 12
     ))
-fig.update_layout(legend=dict(
-    orientation="h",
-    yanchor="bottom",
-    y=1.02,
-    xanchor="right",
-    x=1
+fig.update_layout(legend = dict(
+    orientation = "h",
+    yanchor = "bottom",
+    y = 1.02,
+    xanchor = "right",
+    x = 1
 ))
 
-#Simulate different interest rates
+#Calculate compound interest with varying interest rates
 
 rates = list(np.arange(0.010, 0.110, 0.010))
 rates_res = pd.DataFrame(columns = ['Rate', 'Month', 'Invested', 'Value'])
@@ -57,23 +58,24 @@ for j in range(len(rates)):
             Value = Invested
         else:
          Value = P * ((1 + (rates[j] / 12)) ** i) + M * ((((1 + rates[j] / 12) ** (i + 1)) - (1 + (rates[j] / 12))) / (rates[j] / 12))
-        rates_res =  rates_res.append({'Rate': rates[j], 'Month': Month, 'Invested': Invested, 'Value': Value}, ignore_index = True)
+        rates_res =  rates_res.append({'Rate': round(100 * rates[j], 2), 'Month': Month, 'Invested': Invested, 'Value': Value}, ignore_index = True)
 
-#Plot
+#Plot compound interest with varying interest rates
 fig = px.line(rates_res, x = rates_res["Month"], y = rates_res["Value"],
                     color = "Rate",
                     color_discrete_sequence=px.colors.sequential.Plasma)
 fig.show()
-fig.update_layout(title='Value of Investment, by annual interest rate',
-                   xaxis_title='Months',
-                   yaxis_title='Amount',
-                   font=dict(
-        family="Helvetica",
-        size=12
+fig.update_layout(title = 'Value of investment, by annual interest rate',
+                   xaxis_title = 'Months',
+                   yaxis_title = 'Amount',
+                   legend_title_text = "Interest Rate in %",
+                   font = dict(
+                        family = "Helvetica",
+                        size = 12, 
     ))
 
 
-# Simulate different amounts
+#Calculate compound interest with different amounts
 
 amounts = list(range(100, 1000, 100))
 res = pd.DataFrame(columns = ['Contribution', 'Month', 'Invested', 'Value'])
@@ -87,14 +89,14 @@ for j in range(len(amounts)):
         Invested = P + (amounts[j] * i)
         res =  res.append({'Contribution': amounts[j], 'Month': Month, 'Invested': Invested, 'Value': Value}, ignore_index = True)
 
-#
+#Plot compound interest with different amounts
 fig = px.line(res, x = res["Month"], y = res["Value"],
                     color = "Contribution",
                     color_discrete_sequence=px.colors.sequential.Plasma)
-fig.update_layout(title=str('Value of investment at ' + str(r * 100) + '% interest, per monthly contribution amount'),
-                   xaxis_title='Months',
-                   yaxis_title='Amount',
-                   font=dict(
-        family="Helvetica",
-        size=12
+fig.update_layout(title = str('Value of investment at ' + str(r * 100) + '% interest, per monthly contribution amount'),
+                   xaxis_title = 'Months',
+                   yaxis_title = 'Amount',
+                   font = dict(
+        family = "Helvetica",
+        size = 12
     ))
