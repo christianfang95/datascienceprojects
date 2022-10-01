01b7bc0c4c8dd30bc84e18620f8e9965e3079fe0
Christian Fang <c.zielinski@students.uu.nl>
2022-10-01 14:07:57 +0200

    scripts

---
+++ analyzing-mdpi-papers/scripts/preprocess_data.py
@@ -0,0 +1,21 @@
+import pandas as pd 
+
+
+#import paper links
+papers = pd.read_csv("/Users/christianfang/Documents/GitHub/datascienceprojects/analyzing-mdpi-papers/data/all_papers.csv")
+
+#Exclude duplicates
+papers["identifier"] = papers["identifier"].dropna()
+
+#Get identifiers
+identifiers = papers["identifier"]
+identifiers.head()
+
+#Delete oai: prefix and : between mdpi.com and the links
+identifiers = identifiers.map(lambda x: x.lstrip('oai:').rstrip('/'))
+identifiers = identifiers.str.replace(':','')
+identifiers = 'https://www.' + identifiers
+
+#Write identifiers as csv
+identifiers = pd.DataFrame(identifiers)
+identifiers.to_csv("/Users/christianfang/Documents/GitHub/datascienceprojects/analyzing-mdpi-papers/data/identifiers.csv")
